e6c988e040405cd923eacfe432688a98bc445d60
angelaannrajesh <angelaannrajesh@gmail.com>
2022-08-05 18:56:17 +1200

    Adding Lucky Number Game

--- Coding/dice_rolling_excercse.py
+++ Coding/dice_rolling_excercse.py
@@ -1,3 +1,11 @@
+dice = int(input("How many dice would you like to roll? "))
+side = int(input("How many sides should each die have? "))
+from random import randint
 
+while True:
+    reply = input("Roll again? (q to quit)")
+    print(randint(1,num_sides))
+    if reply == "q":
+        break
 
 
